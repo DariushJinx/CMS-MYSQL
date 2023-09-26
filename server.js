4f1f85f5a7bcaf0ff1f44b591c8bc989a313b155
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-26 21:27:49 +0330

    handle server file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const productRoutes = require("./routes/product.routes");
+const commentRoutes = require("./routes/comment.routes");
+const offRoutes = require("./routes/off.routes");
+const userRoutes = require("./routes/user.routes");
+const orderRoutes = require("./routes/orders.routes");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/offs", offRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.listen(3000);
