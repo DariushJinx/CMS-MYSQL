9c3a4106e1cbce756383d7c2e1bf80231e289a5f
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-29 13:45:51 +0330

    file updated with create new tables

--- DB/shopDB.js
+++ DB/shopDB.js
@@ -38,6 +38,20 @@ connection.changeUser({ database: "Shop" }, (err) => {
   }
 });
 
+let tableNameCategories = "Categories";
+// Query to create table Categories
+let queryCategory = `CREATE TABLE IF NOT EXISTS ${tableNameCategories}
+      (
+        id INT(100) AUTO_INCREMENT PRIMARY KEY,
+        title TEXT(1000) NOT NULL
+      )`;
+
+connection.query(queryCategory, (err, rows) => {
+  if (err) throw err;
+
+  console.log(`Successfully Created Table - ${tableNameCategories}`);
+});
+
 let tableNameProduct = "Products";
 
 // Query to create table Products
@@ -50,7 +64,14 @@ let queryProduct = `CREATE TABLE IF NOT EXISTS ${tableNameProduct}
         img VARCHAR(100) DEFAULT "img.png",
         popularity INT(100) NOT NULL,
         sale INT(100) NOT NULL,
-        colors INT(100) NOT NULL
+        colors INT(100) NOT NULL,
+        description VARCHAR(1000) NOT NULL,
+        url VARCHAR(1000) NOT NULL,
+        categoryID INT(100) NOT NULL,
+        INDEX idx_category (categoryID),
+        CONSTRAINT fk_products_categories
+        FOREIGN KEY(categoryID)
+        REFERENCES Categories(id) ON UPDATE CASCADE ON DELETE RESTRICT
       )`;
 
 connection.query(queryProduct, (err, rows) => {
@@ -92,65 +113,21 @@ let queryComment = `CREATE TABLE IF NOT EXISTS ${tableNameComments}
         date VARCHAR(100) NOT NULL,
         hour VARCHAR(100) NOT NULL,
         userID INT(100) NOT NULL,
+        INDEX idx_user (userID),
+        CONSTRAINT fk_comments_users
         FOREIGN KEY(userID)
-        REFERENCES Users(id),
+        REFERENCES Users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
         productID INT(100) NOT NULL,
+        INDEX idx_product (productID),
+        CONSTRAINT fk_comments_products
         FOREIGN KEY(productID) 
-        REFERENCES Products(id)
+        REFERENCES Products(id) ON UPDATE CASCADE ON DELETE RESTRICT
       )`;
 
 connection.query(queryComment, (err, rows) => {
   if (err) throw err;
 
   console.log(`Successfully Created Table - ${tableNameComments}`);
-});
-
-let tableNameOrders = "Orders";
-
-// Query to create table Orders
-let queryOrders = `CREATE TABLE IF NOT EXISTS ${tableNameOrders} 
-      (
-        id INT(100) AUTO_INCREMENT PRIMARY KEY,
-        userID INT(100) NOT NULL,
-        FOREIGN KEY (userID) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-        productID INT(100) NOT NULL,
-        FOREIGN KEY (productID) REFERENCES Products(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-        date VARCHAR(100) NOT NULL,
-        hour VARCHAR(100) NOT NULL,
-        price BIGINT NOT NULL,
-        off INT(100) NOT NULL,
-        sale BIGINT NOT NULL,
-        popularity INT(100) NOT NULL,
-        count BIGINT NOT NULL,
-        isActive INT(10) NOT NULL DEFAULT 0
-      )`;
-
-connection.query(queryOrders, (err, rows) => {
-  if (err) throw err;
-
-  console.log(`Successfully Created Table - ${tableNameOrders}`);
-});
-
-let tableNameOffs = "Offs";
-
-// Query to create table Offs
-let queryOffs = `CREATE TABLE IF NOT EXISTS ${tableNameOffs} 
-      (
-        id INT AUTO_INCREMENT PRIMARY KEY, 
-        code VARCHAR(100) NOT NULL,
-        percent INT(100) NOT NULL,
-        adminID INT(100) NOT NULL,
-        FOREIGN KEY (adminID) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-        productID INT(100) NOT NULL,
-        FOREIGN KEY (productID) REFERENCES Products(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-        date VARCHAR(100) NOT NULL,
-        isActive INT(1) NOT NULL
-      )`;
-
-connection.query(queryOffs, (err, rows) => {
-  if (err) throw err;
-
-  console.log(`Successfully Created Table - ${tableNameOffs}`);
 });
 
 let tableNameAdmins = "Admins";
@@ -170,6 +147,87 @@ connection.query(queryAdmins, (err, rows) => {
   if (err) throw err;
 
   console.log(`Successfully Created Table - ${tableNameAdmins}`);
+});
+
+let tableNameOrders = "Orders";
+
+// Query to create table Orders
+let queryOrders = `CREATE TABLE IF NOT EXISTS ${tableNameOrders} 
+      (
+        id INT(100) AUTO_INCREMENT PRIMARY KEY,
+        userID INT(100) NOT NULL,
+        INDEX idx_user (userID),
+        CONSTRAINT fk_orders_users
+        FOREIGN KEY (userID) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        productID INT(100) NOT NULL,
+        INDEX idx_product (productID),
+        CONSTRAINT fk_orders_products
+        FOREIGN KEY (productID) REFERENCES Products(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        date VARCHAR(100) NOT NULL,
+        hour VARCHAR(100) NOT NULL,
+        price INT(100) NOT NULL,
+        off INT(100) NOT NULL,
+        sale INT(100) NOT NULL,
+        popularity INT(100) NOT NULL,
+        count INT(100) NOT NULL,
+        isActive INT(10) NOT NULL DEFAULT 0
+      )`;
+
+connection.query(queryOrders, (err, rows) => {
+  if (err) throw err;
+
+  console.log(`Successfully Created Table - ${tableNameOrders}`);
+});
+
+let tableNameOffs = "Offs";
+
+// Query to create table Offs
+let queryOffs = `CREATE TABLE IF NOT EXISTS ${tableNameOffs} 
+      (
+        id INT AUTO_INCREMENT PRIMARY KEY, 
+        code VARCHAR(100) NOT NULL,
+        percent INT(100) NOT NULL,
+        date VARCHAR(100) NOT NULL,
+        isActive INT(1) NOT NULL,
+        adminID INT(100) NOT NULL,
+        FOREIGN KEY(adminID)
+        REFERENCES Admins(id),
+        productID INT(100) NOT NULL,
+        FOREIGN KEY (productID)
+        REFERENCES Products(id)
+      )`;
+
+connection.query(queryOffs, (err, rows) => {
+  if (err) throw err;
+
+  console.log(`Successfully Created Table - ${tableNameOffs}`);
+});
+
+let tableNameBasket = "Baskets";
+
+// Query to create table Baskets
+let queryBasket = `CREATE TABLE IF NOT EXISTS ${tableNameBasket} 
+      (
+        id INT(100) AUTO_INCREMENT PRIMARY KEY, 
+        productID INT(100) NOT NULL,
+        INDEX idx_product (productID),
+        CONSTRAINT fk_baskets_products
+        FOREIGN KEY (productID) REFERENCES Products(id),
+        userID INT(100) NOT NULL,
+        INDEX idx_user (userID),
+        CONSTRAINT fk_baskets_users
+        FOREIGN KEY (userID) REFERENCES Users(id),
+        productImg VARCHAR(100) NOT NULL,
+        productTitle VARCHAR(100) NOT NULL,
+        productPrice INT(100) NOT NULL,
+        productUrl VARCHAR(100) NOT NULL,
+        count INT(100) NOT NULL
+      )`;
+
+connection.query(queryBasket, (err, rows) => {
+  if (err) throw err;
+
+  console.log(`Successfully Created Table - ${tableNameBasket}`);
 });
 
 module.exports = connection;
