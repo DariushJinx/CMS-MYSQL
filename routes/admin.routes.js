a7a358a37c97f946b8452554bc2964f26cc41e2f
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-29 13:46:25 +0330

    fix bug

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,7 +4,7 @@ const adminRoutes = require("express").Router();
 adminRoutes.get("/list", (req, res, next) => {
   try {
     const adminToken = req.headers.authorization;
-    const selectMainAdminQuery = `SELECT * FROM Admins WHERE token=${adminToken}`;
+    const selectMainAdminQuery = `SELECT * FROM Admins WHERE token = '${adminToken}'`;
     connection.query(selectMainAdminQuery, (err, results) => {
       if (err) {
         res.send(null);
